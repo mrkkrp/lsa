4a3d8c1ef5ce4e430b892900ea207ea8539c6955
mrkkrp <markkarpov@opmbx.org>
2015-03-25 23:22:48 +0600

    cosmetic changes

--- src/lsa.h
+++ src/lsa.h
@@ -38,17 +38,17 @@
 
 #define LSA_VERSION "0.1.1"
 #define LSA_LICENSE "LSA - List properties of audio files.\n\n"         \
-  "Copyright (c) 2014, 2015 Mark Karpov\n\n"                                  \
-  "This program is free software: you can redistribute it and/or modify\n" \
-  "it under the terms of the GNU General Public License as published by\n" \
-  "the Free Software Foundation, either version 3 of the License, or\n" \
-  "(at your option) any later version.\n\n"                             \
-  "This program is distributed in the hope that it will be useful,\n"   \
-  "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"    \
-  "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"     \
-  "GNU General Public License for more details.\n\n"                    \
+  "Copyright (c) 2014, 2015 Mark Karpov\n\n"                            \
+  "LSA is free software: you can redistribute it and/or modify it under the\n" \
+  "terms of the GNU General Public License as published by the Free\n"  \
+  "Software Foundation, either version 3 of the License, or (at your\n" \
+  "option) any later version.\n\n"                                      \
+  "LSA is distributed in the hope that it will be useful, but WITHOUT ANY\n" \
+  "WARRANTY; without even the implied warranty of MERCHANTABILITY or\n" \
+  "FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License\n" \
+  "for more details.\n\n"                                               \
   "You should have received a copy of the GNU General Public License\n" \
-  "along with this program.  If not, see <http://www.gnu.org/licenses/>.\n"
+  "along with this program. If not, see <http://www.gnu.org/licenses/>.\n"
 #define LSA_HELP "lsa - list properties of audio files\n\n"             \
   "Usage: lsa [OPTIONS] [DIRECTORY]\n\n"                                \
   "Available options:\n"                                                \
